9499cabac51e7bc1bae563e2fe57711d862983d5
adityaslyf <aditya.varshneymail@gmail.com>
2023-12-16 20:36:19 +0530

    My first backend code

---
+++ index.js
@@ -0,0 +1,18 @@
+    const express = require("express");
+
+    function findSum(a, b, c) {
+    return a + b + c;
+    }
+
+    const app = express();
+
+    app.get("/", function (req, res) {
+     const a = req.query.a;
+     const b = req.query.b;
+     const c = req.query.c;
+     const ans = findSum(a, b, c);
+     res.send(ans.toString());
+   
+    });
+
+    app.listen(3000);
